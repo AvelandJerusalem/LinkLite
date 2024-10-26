6f7ee786d7b19b1c993bed0a0ac8e1d7329e2783
Jeremy Petch <jeremy.petch@outlook.com>
2024-10-26 15:20:07 +0100

    Create initial backend example

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Result};
+use sha256::digest;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(get_url).service(create))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+#[get("/{hash}")]
+async fn get_url(path: web::Path<String>) -> Result<String> {
+    let hash = path.into_inner();
+    Ok(format!("URL is {}", hash))
+}
+
+#[get("/hash/{url}")]
+async fn create(path: web::Path<String>) -> Result<String> {
+    let url = path.into_inner();
+    let hash = digest(url);
+    Ok(format!("Hash is {}", hash))
 }
